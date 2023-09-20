500e987bc79306fb4bdb412a9fab835dad5057a1
joe <okoshi811@gmail.com>
2023-09-20 21:36:57 +0900

    feat: ユーザ情報をAPIの/signupに投げることが出来るようになった

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,6 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Signup: React.FC = () => {
-  return <div>サインアップ</div>
+  
+    const [formData, setFormData] = useState({
+    user_name: '',
+    email: '',
+    password: '',
+  });
+    
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value}));
+  };
+  
+  const handlerSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:8080/signup', formData);
+      console.log(response)
+    } catch (error) {
+      console.log('failed to post data to API', error)
+    }
+  };
+
+  return (
+    <div>
+      <h1>サインアップ</h1>
+      <form onSubmit={handlerSubmit}>
+        name:<input type="text" name="user_name" onChange={handleChange}/><br/>
+        email:<input type="text" name="email" onChange={handleChange}/><br/>
+        password:<input type="password" name="password" onChange={handleChange}/><br/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup
