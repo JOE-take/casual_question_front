d71a58116ba63b90ddd60b3a78bca0c4fe23f9f9
joe <okoshi811@gmail.com>
2023-09-20 20:17:26 +0900

    temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+
+const Home: React.FC = () => {
+  return <div>ホームページ</div>
+}
+
+const Signup: React.FC = () => {
+  return <div>サインアップ</div>
+}
+
+const Login: React.FC = () => {
+  return <div>ログイン</div>
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">ホーム</Link> | <Link to="/signup">サインアップ</Link> | <Link to="/login">ログイン</Link>
+        </nav>
+        
+        <Switch>
+          <Route path="/" Component={Home} />
+          <Route path="/signup" Component={Signup} />
+          <Route path="/login" Component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
