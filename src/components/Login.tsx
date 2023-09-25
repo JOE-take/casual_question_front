d5f03b057712eee1b896b5da9a06a9a0895f1d76
joe <okoshi811@gmail.com>
2023-09-25 23:45:49 +0900

    fix: エラーの条件を変更

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -36,9 +36,6 @@ const Login: React.FC = () => {
       if (axios.isAxiosError(error) && error.response) {
         switch (error.response.status) {
           case 400:
-            seterrMessage('接続失敗, 数分後にもう一度試してください');
-            break;
-          case 401:
             seterrMessage('ログインできません。内容を見直してください。') 
             break;
         }
