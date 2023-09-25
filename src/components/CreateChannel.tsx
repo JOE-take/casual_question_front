cdef0cbac106ac5b3b67c17bf0d16d1073b7b8ae
joe <okoshi811@gmail.com>
2023-09-25 19:37:02 +0900

    ref: トークンが使えない時に無限ループしないように

--- src/components/CreateChannel.tsx
+++ src/components/CreateChannel.tsx
@@ -9,7 +9,7 @@ const CreateChannel: React.FC = () => {
   const { user } = useUser();
   const refreshAccessToken = UseRefreshToken();
 
-  const createChannel = useCallback(async () => {
+  const createChannel = useCallback(async (retryCount = 0) => {
     try {
       console.log(user)
       const response = await axios.post('https://casualquestion.an.r.appspot.com/channel/new', { owner: user.userId }, {
@@ -25,16 +25,24 @@ const CreateChannel: React.FC = () => {
       }
     } catch (error) {
       if (axios.isAxiosError(error) && error.response && error.response.status === 401) {
-        await refreshAccessToken();
-        createChannel();
+        if (retryCount < 3) {
+          await refreshAccessToken();
+          createChannel(retryCount + 1);
+        } else {
+          console.error(error)
+        }
       }
-      console.error(error);
     }
   }, [navigate, user, refreshAccessToken]);
 
+  const handleCreateChannelClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault(); // 必要に応じてデフォルトの動作を防ぐ
+    createChannel(); // 非同期関数を呼び出す
+  };
+
   return (
     <div>
-      <button onClick={createChannel}>新しいChannelを作成</button>
+      <button onClick={handleCreateChannelClick}>新しいChannelを作成</button>
     </div>
   );
 }
