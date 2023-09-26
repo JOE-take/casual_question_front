f8092cc21e452d9adee57c43e632cf063da12093
joe <okoshi811@gmail.com>
2023-09-26 20:15:22 +0900

    fix: Navコンポーネントに関するエラーを修正

---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useUser } from './UserContent'
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  const { user } = useUser();
+
+  if (user && user.userId && user.userName && user.accessToken) {
+    return (
+      <div>
+        <nav>
+          <Link to="/">ホーム</Link>
+          | <Link to="/channel/new">部屋の作成</Link>
+        </nav>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <nav>
+        <Link to="/">ホーム</Link>
+        | <Link to="/signup">サインアップ</Link>
+        | <Link to="/login">ログイン</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav;
