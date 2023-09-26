773c43bb82382d01360efdb84982694a3ed116f6
joe <okoshi811@gmail.com>
2023-09-26 19:46:48 +0900

    ref: ログイン時と非ログイン時でNavバーに出る要素が変わるように

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,50 @@ import Login from './components/Login';
 import Channel from './components/Channel';
 import CreateChannel from './components/CreateChannel';
 import PostQuestion from './components/PostQuestion';
+import { useUser } from './components/UserContent';
 import './App.css';
-
 
 axios.defaults.withCredentials = true;
 function App() {
-  return (
-  <UserProvider>
-    <Router>
+  const { user } = useUser();
+  const drawNav = () => {
+    if (user && user.userId && user.userName && user.accessToken) {
+      return (
+        <div>
+          <nav>
+            <Link to="/">ホーム</Link>
+            | <Link to="/channel/new">部屋の作成</Link>
+          </nav>
+        </div>
+      )
+    }
+    return (
       <div>
         <nav>
-          <Link to="/">ホーム</Link> 
-          | <Link to="/signup">サインアップ</Link> 
+          <Link to="/">ホーム</Link>
+          | <Link to="/signup">サインアップ</Link>
           | <Link to="/login">ログイン</Link>
-          | <Link to ="/channel/new">部屋の作成</Link>
         </nav>
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="channel/new" element={<CreateChannel/>} />
-          <Route path="/channel/:id" element={<Channel />} />
-          <Route path="/:id" element={<PostQuestion />} />
-        </Routes>
       </div>
-    </Router>
-  </UserProvider>
+    )
+  }
+
+  return (
+    <UserProvider>
+      <Router>
+        <div>
+          drawNav();
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="channel/new" element={<CreateChannel />} />
+            <Route path="/channel/:id" element={<Channel />} />
+            <Route path="/:id" element={<PostQuestion />} />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
